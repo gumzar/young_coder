448bdded51ab018cde91e8e180af9472a5f5e220
gumar <gumarzar@gmail.com>
2022-07-23 11:09:12 +0300

    add 3c,4c,5c

---
+++ 3c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  printf("Wake up, Neo...\n");
+  printf("\n");
+  printf("The Matrix has you...\n");
+  printf("\n");
+  printf("Follow the white rabbit...");
+  printf("\n");
+  return 0;
+}
---
+++ 4c.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  printf("NICKNAME: Neo\nCITY: St-Petersburg\nAGE: 35\nHEIGHT: 180\nWEIGHT: 75");
+  printf("\n");
+  return 0;
+}
