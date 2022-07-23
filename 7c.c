b84a577a49f1239c310b01f08b1f32425a4ce5d0
gumar <gumarzar@gmail.com>
2022-07-23 12:56:56 +0300

    add 6-8c.c

---
+++ 7c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void){
+  //while (1 > 0) {
+    printf("Wake up, Neo...\n\n");
+ // }
+  
+  //if (0 > 1){	
+    printf("The Matrix has you...\n\n");
+  //}
+
+  printf("Follow the white rabbit...\n");  
+  return 0;
+}
