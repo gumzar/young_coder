2831807633c8fb74b42e556697814019cc09f088
gumar <gumarzar@gmail.com>
2022-07-23 14:44:50 +0300

    add 9-13c.c

---
+++ 10c.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+int main(void){
+  int a = 25;
+  float b = 23.4;
+  double c = 217.876;
+  char d = 's';
+  
+  printf("Cheloe %d\n",a);
+  printf("Odin simvol %c\n",d);
+  printf("Vewestvennoe %f %g\n",b,c);
+
+  return 0;
+}
---
+++ 13c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf ("S.Holmes:\n");
+  printf ("%dgrad %d\'%d.%d\" N\n", 51,31,25,48);
+   printf("%d grad  %d\'%d.%d\" W\n", 0,9,29,93);
+
+  return 0;
+}
